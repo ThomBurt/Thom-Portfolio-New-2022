3b2aeb901ee7083b3f9933a193262a2ec5bfbaab
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-03-28 14:49:20 -0400

    setting up express

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,4 @@
  https://thomburt.github.io/Thom-Portfolio-New-2022/
+
+
+ https://powerful-temple-53291.herokuapp.com/ | https://git.heroku.com/powerful-temple-53291.git
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+// Listening on PORT
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`),
+  // console.log('Follow this link to get there http://localhost:3001/api/db 🚀')
+);
